831a837f1b657cf895bfe094bf2d951197c483c7
navono <navono007@gmail.com>
2021-03-19 09:39:20 +0800

    fix: Conan.cmake

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,41 +1,19 @@
 if(${PROJECT_NAME}_ENABLE_CONAN)
-  #
-  # Setup Conan requires and options here:
-  #
-
-  set(${PROJECT_NAME}_CONAN_REQUIRES "")
-  set(${PROJECT_NAME}_CONAN_OPTIONS "")
-
-  #
-  # If `conan.cmake` (from https://github.com/conan-io/cmake-conan) does not exist, download it.
-  #
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(
-      STATUS
-        "Downloading conan.cmake from https://github.com/conan-io/cmake-conan..."
-    )
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
-      "${CMAKE_BINARY_DIR}/conan.cmake"
-    )
-    message(STATUS "Cmake-Conan downloaded succesfully.")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
+            "${CMAKE_BINARY_DIR}/conan.cmake")
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-  conan_add_remote(NAME bincrafters 
-      URL
-          https://api.bintray.com/conan/bincrafters/public-conan
-  )
-
   conan_cmake_run(
-    REQUIRES
-      ${CONAN_REQUIRES}
-    OPTIONS
-      ${CONAN_OPTIONS}
-    BASIC_SETUP
-      CMAKE_TARGETS # Individual targets to link to
-    BUILD
-      missing
+          CONANFILE
+          ${PROJECT_SOURCE_DIR}/conanfile.txt
+          BASIC_SETUP
+          CMAKE_TARGETS
+          BUILD
+          missing
   )
 
   verbose_message("Conan is setup and all requires have been installed.")
